5829e07b42a562d83aa2fd2be581c011d6735ecc
Unknown <alessandro.feltrin@barsanti.edu.it>
2019-03-02 09:48:06 +0100

    2/03/19-metodo pulsante e metodo ledTest

--- ProgrammaTestRiflessi/ProgrammaTestRiflessi.ino
+++ ProgrammaTestRiflessi/ProgrammaTestRiflessi.ino
@@ -1,9 +1,39 @@
+int pulsante;
+int ledBlu;
+int bazzer;
+int led_RGB_Rosso;
+int led_RGB_Verde;
+int tempoTestLed;
+int tempoTestBuzzer;
+int primoTempo;
+int secondoTempo;
 void setup() {
   // put your setup code here, to run once:
-
+ 
+}
+void loop() {
+  // put your main code here, to run repeatedly
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void PremiPulsante(int device)
+{
+  bool finito=false;
+  digitalWrite(device,HIGH);
+  while(!finito)
+  {
+    if(pulsante==HIGH)
+    {
+      finito=true;
+    }
+  }
+  digitalWrite(device,LOW);
+}
 
+void LedTest()
+{
+  delay(random(1000,10000));
+  primoTempo=millis();
+  PremiPulsante(ledBlu);
+  secondoTempo=millis();
+  tempoTestLed=secondoTempo-primoTempo;
 }
