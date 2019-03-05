f1a3b0afa37cfb0029e93cba5ee8d4b9c6002f20
AlessandroFeltrin <alessandro.feltrin@barsanti.edu.it>
2019-03-05 16:37:02 +0100

    05/03/2019-modificati i metodi test e pulsante

--- ProgrammaTestRiflessi/ProgrammaTestRiflessi.ino
+++ ProgrammaTestRiflessi/ProgrammaTestRiflessi.ino
@@ -5,20 +5,16 @@ int led_RGB_Rosso;
 int led_RGB_Verde;
 int tempoTestLed;
 int tempoTestBuzzer;
-int primoTempo;
-int secondoTempo;
 void setup() {
-  // put your setup code here, to run once:
- 
+
 }
 void loop() {
-  // put your main code here, to run repeatedly
+
 }
 
-void PremiPulsante(int device)
+void PremiPulsante()
 {
   bool finito=false;
-  digitalWrite(device,HIGH);
   while(!finito)
   {
     if(pulsante==HIGH)
@@ -26,14 +22,19 @@ void PremiPulsante(int device)
       finito=true;
     }
   }
-  digitalWrite(device,LOW);
 }
 
-void LedTest()
+int Test(int device)
 {
+  int primoTempo;
+  int secondoTempo;
+  int tempoTest;
   delay(random(1000,10000));
+  digitalWrite(device,HIGH);
   primoTempo=millis();
-  PremiPulsante(ledBlu);
+  PremiPulsante();
+  digitalWrite(device,LOW);
   secondoTempo=millis();
-  tempoTestLed=secondoTempo-primoTempo;
+  tempoTest=secondoTempo-primoTempo;
+  return tempoTest;
 }
